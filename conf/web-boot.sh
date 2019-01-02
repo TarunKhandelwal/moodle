12f5bdb47bc2a4398d5ac75eb117c34618f91504
Tarun Khandelwal <tarun.khandelwal@hp.com>
2019-01-03 02:04:11 +0530

    Web COnfiguration

--- conf/web-boot.sh
+++ conf/web-boot.sh
@@ -1,3 +1,5 @@
+find . -name "access_log"
+ls -a
 # echo "Installing PECL APC"
 # /app/php/bin/pecl install apc
 
@@ -6,15 +8,15 @@
 # echo "[memcache]" >> /app/php/php.ini
 # echo "extension=memcache.so" >> /app/php/php.ini
 
-sed -i 's/Listen 80/Listen '$PORT'/' /app/apache/conf/httpd.conf
+sed -i 's/Listen 80/Listen '$PORT'/' /app/conf/httpd.conf
 # sed -i 's/^DocumentRoot/# DocumentRoot/' /app/apache/conf/httpd.conf
 # sed -i 's/^ServerLimit 1/ServerLimit 8/' /app/apache/conf/httpd.conf
 # sed -i 's/^MaxClients 1/MaxClients 8/' /app/apache/conf/httpd.conf
 
 for var in `env|cut -f1 -d=`; do
-  echo "PassEnv $var" >> /app/apache/conf/httpd.conf;
+  echo "PassEnv $var" >> /app/conf/httpd.conf;
 done
-echo "Include /app/conf/httpd/*.conf" >> /app/apache/conf/httpd.conf
+echo "Include /app/conf/httpd/*.conf" >> /app//conf/httpd.conf
 touch /app/apache/logs/error_log
 touch /app/apache/logs/access_log
 tail -F /app/apache/logs/error_log &
