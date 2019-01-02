4ae5c9bb7572ef90b189e6f145905072add1b40f
Tarun Khandelwal <tarun.khandelwal@hp.com>
2019-01-03 01:12:23 +0530

    Updating db configuration

--- lib/dml/pgsql_native_moodle_database.php
+++ lib/dml/pgsql_native_moodle_database.php
@@ -167,6 +167,7 @@ class pgsql_native_moodle_database extends moodle_database {
 
         ob_start();
         if (empty($this->dboptions['dbpersist'])) {
+			echo($connection);
             $this->pgsql = pg_connect($connection, PGSQL_CONNECT_FORCE_NEW);
         } else {
             $this->pgsql = pg_pconnect($connection, PGSQL_CONNECT_FORCE_NEW);
